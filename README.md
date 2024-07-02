407caeda9f8e8e1d581dcd7f120a6013ef7e744a
Luciano Matos <lucianommatos@gmail.com>
2024-07-02 23:10:30 +0100

    Add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-clone
+
 Project created and developed for practical exercises of the course: "curso.dev"
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
- function Home() {
-    return <h1>❤️Thais, obrigado por ser a melhor mãe do mundo para nossa filha. Te amo!!!❤️</h1>
- }
- 
- export default Home;
+function Home() {
+  return (
+    <h1>
+      ❤️Thais, obrigado por ser a melhor mãe do mundo para nossa filha. Te
+      amo!!!❤️
+    </h1>
+  );
+}
+
+export default Home;
